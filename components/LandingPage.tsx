36629d10b9490347e0c2e2efece64bdfd53bfdcf
Aryan Srivastava <74702929+aryan-srivastavaa@users.noreply.github.com>
2025-10-08 11:42:15 +0530

    update contact and resume dark font

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -47,21 +47,23 @@ function LandingPage() {
                         </a>
                     </div>
                     <div className="flex space-x-4 mt-6 flex-wrap gap-1">
-                        <a
-                            href="/contact"
-                            className="relative inline-block px-6 py-2 text-white font-semibold rounded-md bg-gradient-to-r from-blue-500 to-cyan-500 dark:bg-gradient-to-r from-blue-500 to-cyan-500 shadow-md transform transition hover:scale-105 hover:shadow-xl"
-                        >
-                            Contact Me
-                        </a>
-                        <a
-                            href="https://drive.google.com/file/d/1-bIY_rGsavWgy2tSPW3egxQs7n_e4ms5/view?usp=sharing"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="relative inline-block px-6 py-2 font-semibold text-blue-600 border-2 border-blue-500 rounded-md bg-white shadow-md transition transform hover:bg-gradient-to-r hover:from-blue-500 hover:to-cyan-500 dark: hover:bg-gradient-to-r hover:from-blue-500 hover:to-cyan-500 hover:text-white hover:scale-105 hover:shadow-xl"
-                        >
-                            View Resume
-                        </a>
-                    </div>
+  <a
+    href="/contact"
+    className="relative inline-block px-6 py-2 text-white font-semibold rounded-md bg-gradient-to-r from-blue-500 to-cyan-500 shadow-md transform transition hover:scale-105 hover:shadow-xl"
+  >
+    Contact Me
+  </a>
+  <a
+    href="https://drive.google.com/file/d/1-bIY_rGsavWgy2tSPW3egxQs7n_e4ms5/view?usp=sharing"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="relative inline-block px-6 py-2 font-semibold text-blue-600 border-2 border-blue-500 rounded-md bg-white shadow-md transition transform hover:bg-gradient-to-r hover:from-blue-500 hover:to-cyan-500 hover:text-white hover:scale-105 hover:shadow-xl 
+               dark:bg-white dark:text-blue-600 dark:border-blue-500 dark:hover:text-blue-600" 
+  >
+    View Resume
+  </a>
+</div>
+
 
                 </div>
                 <div className='w-2/5 cursor-pointer' >
